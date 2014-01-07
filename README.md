52d12899d22c511e0bfdb3af619efdcabda1067d
anromeo <anthonykzullo@gmail.com>
2014-01-06 18:07:51 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A Website for Garth Nix
+=======================
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GarthNix::Application.config.secret_key_base = '29913a43803723a41d9e1bab5ae016989369954d0dcd7c932491964617c83af3f9f082f48cd0f1fd9e4b44d60e6568b4ca66f1068f06a5d7f8ac4ad38a349409'
