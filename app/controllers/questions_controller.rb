32772d0c3ccecec943dd69450ffa26e6156c6b95
anromeo <anthonykzullo@gmail.com>
2014-01-10 23:59:11 -0800

    form for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,17 @@
 class QuestionsController < ApplicationController
 
   def index
-    
+    @error_messages = flash[:notice]
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params[:question])
+    if @question.save
+      flash[:notice] = "Your question has been asked. Stay tuned for an answer."
+    else
+      flash[:notice] = "There was a problem. Please, try again later."
+    end
+    render action: "index"
   end
 end
