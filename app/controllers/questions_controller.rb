389255e1fb79ddf17eeb0ba0581d50aa2524b5f0
anromeo <anthonykzullo@gmail.com>
2014-01-11 23:37:12 -0800

    question create finished

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,15 +3,25 @@ class QuestionsController < ApplicationController
   def index
     @error_messages = flash[:notice]
     @question = Question.new
+    @questions = Question.all
   end
 
   def create
-    @question = Question.new(params[:question])
+    @question = Question.new(question_params)
     if @question.save
       flash[:notice] = "Your question has been asked. Stay tuned for an answer."
     else
       flash[:notice] = "There was a problem. Please, try again later."
     end
-    render action: "index"
+    render action: "thanks"
+  end
+
+  def thanks
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:name, :q, :email)
   end
 end
